741395cca302e7e6084337d6e179846659b45292
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-04-16 12:53:48 -0400

    finished with working test

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+    public static String charReplacement(String input) {
+
+        String str = input
+                .replace("f", "7")
+                .replace("F", "7")
+                .replace("s", "$")
+                .replace("S", "$")
+                .replace("1", "!")
+                .replace("a", "@")
+                .replace("A", "@");
+
+        return str;
+
+    }
 }
+
+
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
 public class Problem1Test {
+
+
+    @Test
+    public void testCharReplacement(){
+        //given
+       String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+       //when
+       String actual = Problem1.charReplacement(input);
+       String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+       //then
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
